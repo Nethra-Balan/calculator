0d7d3ed73ac05efbf537ee8441bb2980ed2c8422
Nethra-Balan <nethrabalan2005@gmail.com>
2025-01-20 21:22:10 +0530

    add functionalities

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+let exp="";
+let prevop='';
 function copy(id){
+    let str=document.getElementById("text").value;
+    if(str.length>10){
+        return;
+    }
+    if(prevop){
+    document.getElementById(prevop).style.color='white';
+    document.getElementById(prevop).style.backgroundColor='orange';
+    document.getElementById("text").value="";
+    prevop='';
+    }
     document.getElementById("text").value+=id;
+    exp+=id;
+}
+function sign(id){
+    if(prevop){
+        document.getElementById(prevop).style.color='white';
+        document.getElementById(prevop).style.backgroundColor='orange';
+        document.getElementById("text").value="";
+    }
+    document.getElementById(id).style.color='orange';
+    document.getElementById(id).style.backgroundColor='white';
+    // document.getElementById("text").value+=id;
+    exp+=id;
+    prevop=id;
 }
 function clearInput(){
     document.getElementById("text").value="";
+    exp="";
+}
+function solve(){
+    document.getElementById("text").value = math.evaluate(exp);
+}
+function changesign(){
+        let num=Number(document.getElementById("text").value);
+        document.getElementById("text").value=-num;
+        exp=-num;
+}
+function percent(){
+    exp /= 100;
+    document.getElementById("text").value=exp;
 }
